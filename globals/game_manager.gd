90d65b7937c9a0ab81b7a01b3645c3770d056267
StarryNight <188835614+StarryNightDev@users.noreply.github.com>
2024-11-18 14:33:39 -0800

    feat: Jerrified 2

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -5,3 +5,19 @@ var player: Node
 func set_player(playerNode):
 	player = playerNode
 	
+func set_timeout(callback: Callable, delay: float) -> void:
+	# Create a one-shot timer
+	var timer := Timer.new()
+	timer.one_shot = true
+	timer.wait_time = delay
+	add_child(timer)  # Add the timer to the scene tree
+	timer.start()
+
+	# Wait for the timer to finish
+	await timer.timeout
+
+	# Call the provided callback
+	callback.call()
+
+	# Remove the timer from the scene tree
+	timer.queue_free()
--- scenes/player/player_controller.gd
+++ scenes/player/player_controller.gd
@@ -1,19 +1,35 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
+const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 
-@onready var animation_player: AnimationPlayer = $Jerry/Jelly_V1/Armature/AnimationPlayer
+const IDLE_ANIMATION = 'jelly idle'
+const WALK_ANIMATION = 'jelly walk'
+const JUMP_ANIMATION = 'jelly jump'
+const RUN_ANIMATION = 'jelly run'
+
+@onready var animation_player: AnimationPlayer = $Jerry/Jelly_V1/AnimationPlayer
 @onready var jerry: Node3D = $Jerry
 @onready var camera_attach: Node3D = $CameraAttach
 
 var isMoving = false
+var wasSprinting = false
+var isSprinting = false
 
 func _ready() -> void:
 	GameManager.set_player(self)
-	animation_player.set_blend_time('idle', 'walk', 0.2)
-	animation_player.set_blend_time('walk', 'idle', 0.2)
+	animation_player.play(IDLE_ANIMATION)
+	animation_player.set_blend_time(IDLE_ANIMATION, WALK_ANIMATION, 0.2)
+	animation_player.set_blend_time(WALK_ANIMATION, IDLE_ANIMATION, 0.2)
+	animation_player.set_blend_time(RUN_ANIMATION, WALK_ANIMATION, 0.2)
+	animation_player.set_blend_time(RUN_ANIMATION, IDLE_ANIMATION, 0.2)
+	
+	animation_player.set_blend_time(JUMP_ANIMATION, IDLE_ANIMATION, 0.2)
+	animation_player.set_blend_time(JUMP_ANIMATION, WALK_ANIMATION, 0.2)
+	animation_player.set_blend_time(WALK_ANIMATION, JUMP_ANIMATION, 0.2)
+	animation_player.set_blend_time(IDLE_ANIMATION, JUMP_ANIMATION, 0.2)
+	
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,25 +38,46 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		animation_player.play(JUMP_ANIMATION)
+		isMoving = false
+		
+	isSprinting = Input.is_action_pressed('sprint')
+	var useSpeed = SPRINT_SPEED if wasSprinting else SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Get the camera attaches rotation around the character to apply the proper movement direction (in direction of the camera)
+	var applyRotation = camera_attach.basis.get_euler().y
+	
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).rotated(Vector3.UP, applyRotation).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * useSpeed
+		velocity.z = direction.z * useSpeed
 		
-		jerry.look_at(direction + position)
+		jerry.look_at(-direction + position)
 		
 		if !isMoving:
 			isMoving = true
-			animation_player.play('walk')
+			if !isSprinting:
+				animation_player.play(WALK_ANIMATION)
+				wasSprinting = false
+			else:
+				animation_player.play(RUN_ANIMATION)
+				wasSprinting = true
+		else:
+			if isSprinting and !wasSprinting:
+				wasSprinting = true
+				animation_player.play(RUN_ANIMATION)
+			if !isSprinting and wasSprinting:
+				wasSprinting = false
+				animation_player.play(WALK_ANIMATION)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		if isMoving:
 			isMoving = false
-			animation_player.play('idle')
+			wasSprinting = false
+			animation_player.play(IDLE_ANIMATION)
 
 	move_and_slide()
